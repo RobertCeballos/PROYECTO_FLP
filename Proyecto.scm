b401f08d075bcb262620c1a8edeae978bd3cd790
Juan David Ospina Vasquez <juandaosp12@gmail.com>
2012-11-09 12:37:16 -0500

    BNF

--- Proyecto.scm
+++ Proyecto.scm
@@ -14,7 +14,9 @@
 ;;                      <a-program (exp)>
 ;;  <expression>    ::= <variable>
 ;;  <variable>      ::= <upper-case-letter> (letter | <digit>)*
+;;                      <normal-var-exp (var)> 
 ;;                  ::= _ (<letter> | <digit>)*
+;;                      <anonimous-var-exp(var)>
 ;;  <letter>        ::= <upper-case-letter> | <lower-case-letter>
 ;;  <upper-case-letter> ::= A|B|C|D|E|F|G|H|I|J|K|L|M|N|Ñ|O|P|Q|R|S|T|U|V|W|X|Y|Z                
 ;;  <lower-case-letter> ::= a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z  
