7a729fe6be4b02ad3a750ea7f5729c4b941e0daf
Carolina Crespo Ch <karito0529@gmail.com>
2012-11-10 04:54:46 -0500

    Aqui esta el estado actual del proyecto, me toco hacerle muchas modificaciones a lo que habia subido inicialmente, para poderlo adaptar a la forma en que el profe nos explico, no trabaje sobre la version de roberto, ni mezcle  pq eran muchos los cambios y en la version vieja hay cosas q no vamos a usar... estamos hablando

---
+++ estadoproyecto.scm
@@ -0,0 +1,36 @@
+;ESTADO ACTUAL PROYECTO
+
+;2.1.1 Programa -> ya definido
+
+;2.1.2 Cuerpo -> ya definido 
+
+;2.2.3 Variables -> ya existen las variables asociadas a un respectivo store y ambiente, falta el trato de variables anonimas
+
+;2.1.4 Creacion de variables locales -> ya definidas
+
+;2.1.5 Asignacion -> ya definida funciona para set X=3 set X=Y set 3=X
+
+;2.1.6 Operaciones sobre Varibles-> ya definidas isdet?, isfree?
+
+;2.1.7 Números -> ya estan definidos los numeros enteros y flotantes
+
+;2.1.8 Registros ->
+
+;2.1.9 Funciones y procedimientos->
+
+;2.1.10 Ejecucion de condicionales->
+
+;2.1.11 Especiales->
+
+;2.1.12 Primitivas -> Modificacion a la forma en que se operaba anteriormente, por tanto ya no tiene la restriccion de operar entre flotantes y negativos ( la forma que habia planteado anteriormente, tenia un error)
+;    Operaciones aritmeticas -> ya definidas
+;    Comparacion -> ya definido
+;    Logicas-> ya definidas
+;    Unificacion->
+;    Operaciones sobre puertos->
+;    Operacines sobre celdas->
+
+;2.1.13 Unificacion->
+
+;2.1.12 Ciclos ->
+
