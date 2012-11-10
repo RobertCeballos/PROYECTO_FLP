e11529cd21b7c59ad5e0981caa3206bc54240390
Carolina Crespo Ch <karito0529@gmail.com>
2012-11-10 04:57:26 -0500

    Archivo adicional que es usado por proyecto-10-11

---
+++ variables.scm
@@ -0,0 +1,30 @@
+(define-datatype variable variable?
+  (a-variable (serial number?)
+              (valor list?))) 
+
+
+(define create-var
+  (lambda (serial valor)
+    (a-variable serial (list valor))))
+
+
+(define get-serial
+  (lambda (var)
+    (cases variable var
+      (a-variable (serial valor) serial))))
+
+(define get-valor
+  (lambda (var)
+    (cases variable var
+      (a-variable (serial valor) valor))))
+
+(define isFree?
+  (lambda (var)
+    (cases variable var
+      (a-variable (serial valor)
+                 (if (equal? (car valor) '_) #t #f)))))
+
+(define isDet?
+  (lambda (var)
+    (not (isFree? var))))
+
